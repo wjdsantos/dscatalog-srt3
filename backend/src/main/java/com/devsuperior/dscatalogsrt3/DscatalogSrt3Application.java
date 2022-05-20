c330fd5943f3f4a2cd45c288323b6c94aa255c39
wjdsantos <walter4asantos@gmail.com>
2022-05-20 10:44:38 -0300

    Backend project created

---
+++ backend/src/main/java/com/devsuperior/dscatalogsrt3/DscatalogSrt3Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalogsrt3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogSrt3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogSrt3Application.class, args);
+	}
+
+}
