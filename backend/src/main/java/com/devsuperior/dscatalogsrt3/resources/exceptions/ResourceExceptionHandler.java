ee225c85b018652f870559d51d40aae1cda3d4cd
wjdsantos <walter4asantos@gmail.com>
2022-05-23 16:47:00 -0300

    POST new Category

--- backend/src/main/java/com/devsuperior/dscatalogsrt3/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalogsrt3/resources/exceptions/ResourceExceptionHandler.java
@@ -19,7 +19,7 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
 		err.setStatus(HttpStatus.NOT_FOUND.value());
-		err.setError("Resource nor found");
+		err.setError("Resource not found");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
