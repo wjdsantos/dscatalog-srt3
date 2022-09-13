b5dc363262b720bbaf78c737a5d74dac6471b9f7
wjdsantos <walter4asantos@gmail.com>
2022-09-13 18:02:58 -0300

    Navbar styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
-import './assets/styles/custom.scss';
-import './App.css';
+import Navbar from "./components/Navbar";
+import "./assets/styles/custom.scss";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <h1>Hello DSCatalog srt3</h1>
+    <>
+      <Navbar />
+      <h1>Hello DSCatalog srt3</h1>
+    </>
   );
-}
+};
 
 export default App;
