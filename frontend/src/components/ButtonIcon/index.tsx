d0389bce646a97662b13b2f99b33d249d0047254
wjdsantos <walter4asantos@gmail.com>
2022-09-16 11:05:35 -0300

    Custom ButtonIcon

--- frontend/src/components/ButtonIcon/index.tsx
+++ frontend/src/components/ButtonIcon/index.tsx
@@ -1,11 +1,20 @@
-import './styles.css';
+import "./styles.css";
+
+import { ReactComponent as ArrowBoxIcon } from "assets/images/arrow-box.svg";
+//import { ReactComponent as ArrowIcon } from "assets/images/arrow.svg";
 
 const ButtonIcon = () => {
-    return (
-        <button className="btn btn-primary btn-icon">
-            <h6>INICIE AGORA A SUA BUSCA</h6>
-        </button>
-    );
-}
+  return (
+    <div className="btn-container">
+      <button className="btn btn-primary">
+        <h6>INICIE AGORA A SUA BUSCA</h6>
+      </button>
+      <div className="btn-icon-container">
+        <ArrowBoxIcon />
+{/*        <ArrowIcon /> */}
+      </div>
+    </div>
+  );
+};
 
 export default ButtonIcon;
