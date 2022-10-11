6929733f06f157f8a24bdec0fc95fde2f04c0d78
wjdsantos <walter4asantos@gmail.com>
2022-10-11 17:20:36 -0300

    Banner styling

--- frontend/src/pages/Admin/Auth/index.tsx
+++ frontend/src/pages/Admin/Auth/index.tsx
@@ -1,11 +1,13 @@
 import { ReactComponent as AuthImage } from 'assets/images/auth-image.svg';
 import { Route, Switch } from 'react-router-dom';
 
+import './styles.css';
+
 const Auth = () => {
 
     return (
         <div className="auth-container">
-            <div className="auth-xxx-container">
+            <div className="auth-banner-container">
                 <h1>Divulgue seus produtos no DS Catalog</h1>
                 <p>Faça parte do nosso catálogo de divulgação e aumente a venda dos seus produtos.</p>
                 <AuthImage />
