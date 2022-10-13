2e94835e88bbeaf332c230e723da460b10131cd3
wjdsantos <walter4asantos@gmail.com>
2022-10-13 18:47:05 -0300

    Login request

--- frontend/src/pages/Admin/Auth/Login/index.tsx
+++ frontend/src/pages/Admin/Auth/Login/index.tsx
@@ -2,58 +2,64 @@ import ButtonIcon from "components/ButtonIcon";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
-import './styles.css';
+import "./styles.css";
+import { requestBackendLogin } from "util/requests";
 
 type FormData = {
-    username: string;
-    password: string;
-}
+  username: string;
+  password: string;
+};
 
 const Login = () => {
+  const { register, handleSubmit } = useForm<FormData>();
 
-    const { register, handleSubmit } = useForm<FormData>();
+  const onSubmit = (formData: FormData) => {
+    requestBackendLogin(formData)
+      .then((response) => {
+        console.log("SUCESSO", response);
+      })
+      .catch((error) => {
+        console.log("ERRO", error);
+      });
+  };
 
-    const onSubmit = (formData : FormData) => {
-        console.log(formData);
-    }
-
-    return (
-        <div className="base-card login-card">
-            <h1>LOGIN</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="mb-4">
-                    <input 
-                        {...register("username")}
-                        type="text"
-                        className="form-control base-input"
-                        placeholder="Email"
-                        name="username"
-                     />
-                </div>
-                <div className="mb-2">
-                    <input 
-                        {...register("password")}
-                        type="password"
-                        className="form-control base-input"
-                        placeholder="Password"
-                        name="password"
-                     />
-                </div>
-                <Link to="/admin/auth/recover" className="login-link-recover">
-                    Esqueci a senha
-                </Link>
-                <div className="login-submit">
-                    <ButtonIcon text="FAZER LOGIN" />
-                </div>
-                <div className="signup-container">
-                    <span className="not-registered">Não tem cadastro?</span>
-                    <Link to="/admin/auth/signup" className="login-link-signup">
-                        CADASTRAR
-                    </Link>
-                </div>
-            </form>
+  return (
+    <div className="base-card login-card">
+      <h1>LOGIN</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-4">
+          <input
+            {...register("username")}
+            type="text"
+            className="form-control base-input"
+            placeholder="Email"
+            name="username"
+          />
         </div>
-    );
-}
+        <div className="mb-2">
+          <input
+            {...register("password")}
+            type="password"
+            className="form-control base-input"
+            placeholder="Password"
+            name="password"
+          />
+        </div>
+        <Link to="/admin/auth/recover" className="login-link-recover">
+          Esqueci a senha
+        </Link>
+        <div className="login-submit">
+          <ButtonIcon text="FAZER LOGIN" />
+        </div>
+        <div className="signup-container">
+          <span className="not-registered">Não tem cadastro?</span>
+          <Link to="/admin/auth/signup" className="login-link-signup">
+            CADASTRAR
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
