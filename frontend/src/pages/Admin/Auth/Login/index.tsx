659a54b7aaf7e4349ecd75b13ff57876b3cb3f3c
wjdsantos <walter4asantos@gmail.com>
2022-10-14 17:33:55 -0300

    Saving auth data to localStorage

--- frontend/src/pages/Admin/Auth/Login/index.tsx
+++ frontend/src/pages/Admin/Auth/Login/index.tsx
@@ -1,7 +1,7 @@
 import ButtonIcon from "components/ButtonIcon";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import { requestBackendLogin } from "util/requests";
+import { getAuthData, requestBackendLogin, saveAuthData } from "util/requests";
 import { useState } from "react";
 
 import "./styles.css";
@@ -19,6 +19,9 @@ const Login = () => {
   const onSubmit = (formData: FormData) => {
     requestBackendLogin(formData)
       .then((response) => {
+        saveAuthData(response.data);
+        const token = getAuthData().access_token;
+        console.log('TOKEN Gerado: ' + token);
         setHasError(false);
         console.log("SUCESSO", response);
       })
--- frontend/src/util/requests.ts
+++ frontend/src/util/requests.ts
@@ -1,6 +1,17 @@
 import axios, { Axios } from 'axios';
 import qs from 'qs';
 
+type LoginResponse = {
+    access_token: string;
+    token_type: string;
+    expires_in: number;
+    scope: string;
+    userFirstName: string;
+    userId: number;
+}
+
+const tokenKey = 'authData';
+
 export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog-srt3';
@@ -26,4 +37,13 @@ export const requestBackendLogin = (loginData : LoginData) => {
     });
 
     return axios({method: 'POST', baseURL: BASE_URL, url: '/oauth/token', data, headers});
+}
+
+export const saveAuthData = (obj : LoginResponse) => {
+    localStorage.setItem(tokenKey, JSON.stringify(obj));
+}
+
+export const getAuthData = () => {
+    const str = localStorage.getItem(tokenKey) ?? "{}";
+    return JSON.parse(str) as LoginResponse;
 }
