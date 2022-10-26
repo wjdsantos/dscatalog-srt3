261d9bc31fe9ba95819f5126a3c56417f4f80d7a
wjdsantos <walter4asantos@gmail.com>
2022-10-26 19:16:43 -0300

    Global state with Context API

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -5,36 +5,31 @@ import {
   getTokenData,
   isAuthenticated,
   removeAuthData,
-  TokenData,
 } from "util/requests";
-import { useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import history from "util/history";
-
-type AuthData = {
-  authenticated: boolean;
-  tokenData?: TokenData;
-};
+import { AuthContext } from "AuthContext";
 
 const App = () => {
-  const [authData, setAuthData] = useState<AuthData>({ authenticated: false });
+  const {authContextData, setAuthContextData} = useContext(AuthContext);
 
   useEffect(() => {
     if (isAuthenticated()) {
-      setAuthData({
+      setAuthContextData({
         authenticated: true,
         tokenData: getTokenData(),
       });
     } else {
-      setAuthData({
+      setAuthContextData({
         authenticated: false,
       });
     }
-  }, []);
+  }, [setAuthContextData]);
 
   const handleLogoutClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
     event.preventDefault();
     removeAuthData();
-    setAuthData({
+    setAuthContextData({
       authenticated: false,
     });
     history.replace("/");
@@ -80,9 +75,9 @@ const App = () => {
         </div>
 
         <div className="nav-login-logout">
-          {authData.authenticated ? (
+          {authContextData.authenticated ? (
             <>
-              <span className="nav-username">{authData.tokenData?.user_name}</span>
+              <span className="nav-username">{authContextData.tokenData?.user_name}</span>
               <a href="#logout" onClick={handleLogoutClick}>
                 LOGOUT
               </a>
