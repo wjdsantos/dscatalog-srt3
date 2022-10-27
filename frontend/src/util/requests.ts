05330aabb2330aa5f070c7dde23ad8d15c08b35a
wjdsantos <walter4asantos@gmail.com>
2022-10-27 13:17:29 -0300

    hasAnyRoles

--- frontend/src/util/requests.ts
+++ frontend/src/util/requests.ts
@@ -112,3 +112,27 @@ export const isAuthenticated = () : boolean => {
   const tokenData = getTokenData();
   return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
 }
+
+export const hasAnyRoles = (roles: Role[]) : boolean => {
+
+  if (roles.length === 0) {
+    return true;
+  }
+
+  const tokenData = getTokenData();
+
+  //==>Pode ser resolvido como abaixo (função de alta ordem)
+  //if (tokenData !== undefined) {
+  //  return roles.some(role => tokenData.authorities.includes(role));
+  //}
+
+  if (tokenData !== undefined) {
+    for (var i = 0; i < roles.length; i++) {
+      if (tokenData.authorities.includes(roles[i])) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
